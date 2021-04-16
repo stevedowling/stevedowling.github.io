4e80f3c65ef5884e31b532a50b48b236affb106c
stevedowling <steve@dowling.co.nz>
2021-04-16 09:49:51 +0000

    Published from github actions

--- js/HelpWidget.js
+++ js/HelpWidget.js
@@ -1,12 +1,17 @@
 var HelpWidget = /** @class */ (function () {
     function HelpWidget() {
         var _this = this;
-        this._debug = true;
+        this.DefaultHelpUrl = 'https://faq.cvcheck.com';
         console.log("Constructor called");
         this._iframe = document.createElement('iframe');
         this._iframe.id = 'HelpIframe';
+        this._iframe.addEventListener("load", function (e) { _this.onIframeLoaded(e, _this._closeHelpIframe); }, false);
         this._closeHelpIframe = document.createElement('div');
         this._closeHelpIframe.id = 'CloseHelpIframe';
+        this._closeHelpIframe.innerHTML = 'X';
+        this._closeHelpIframe.style.display = 'none';
+        document.body.appendChild(this._closeHelpIframe);
+        this._closeHelpIframe.addEventListener("click", function () { _this.hideHelp(); }, false);
         this._helpButton = document.createElement('button');
         this._helpButton.id = 'ShowHelp';
         this._helpButton.innerHTML = 'HELP';
@@ -14,10 +19,10 @@ var HelpWidget = /** @class */ (function () {
         this._helpButton.addEventListener('click', function () { _this.showHelp(); }, false);
     }
     HelpWidget.prototype._getHelpUrl = function () {
-        // TODO: Add some logic here to return the right iFrame source 
-        var iframeUrl = 'TestPage.html';
-        var currentLocation = window.location.href; // Use this to change the iFrame source
-        var iframeSourceOverride = document.getElementById('IframeSourceOverride').value;
+        var iframeUrl = this.DefaultHelpUrl;
+        var currentUrlOverride = document.querySelector('input[name="sourceText"]:checked').value;
+        ; // TODO: Remove this after testing
+        var iframeSourceOverride = HelpWidget.HelpUrlOverrideDictionary[currentUrlOverride !== null && currentUrlOverride !== void 0 ? currentUrlOverride : window.location.href];
         if (iframeSourceOverride) {
             iframeUrl = iframeSourceOverride;
         }
@@ -25,20 +30,26 @@ var HelpWidget = /** @class */ (function () {
         console.log("iframeUrl: " + iframeUrl);
         return iframeUrl;
     };
+    HelpWidget.prototype.onIframeLoaded = function (e, closeButton) {
+        //TODO: Detect if scrollbar present and change right position
+        //      This doesn't work:
+        // if (document.body.scrollHeight > document.body.clientHeight) {
+        //     closeButton.style.right = '17px';
+        // } else {
+        //     closeButton.style.right = '0';
+        // }
+        closeButton.style.display = 'block';
+    };
     HelpWidget.prototype.showHelp = function () {
-        var _this = this;
         console.log("showHelp called");
         this.hideHelpButton();
         this._iframe.src = this._getHelpUrl();
         document.body.appendChild(this._iframe);
-        this._closeHelpIframe.innerHTML = 'X';
-        this._closeHelpIframe.addEventListener("click", function () { _this.hideHelp(); }, false);
-        document.body.appendChild(this._closeHelpIframe);
     };
     HelpWidget.prototype.hideHelp = function () {
         console.log("hideHelp called");
         this._iframe.remove();
-        this._closeHelpIframe.remove();
+        this._closeHelpIframe.style.display = 'none';
         this.showHelpButton();
     };
     HelpWidget.prototype.showHelpButton = function () {
@@ -48,6 +59,11 @@ var HelpWidget = /** @class */ (function () {
     HelpWidget.prototype.hideHelpButton = function () {
         console.log("hideHelpButton called");
         this._helpButton.remove();
+    };
+    HelpWidget.HelpUrlOverrideDictionary = {
+        "NPC": "https://faq.cvcheck.com/category/national-police-check-australia/",
+        "About": "https://faq.cvcheck.com/category/about-cvcheck/",
+        "Employers": "https://faq.cvcheck.com/category/employers/"
     };
     return HelpWidget;
 }());
